fa47fdb9c24379389a7b7eb2b54dec4dfa6a2365
Sam Fearn <sam.fearn@gmail.com>
2020-07-16 23:30:10 +0100

    Commented the main function of divit.lua, as well as added code to handle the case of no label.

--- divit.lua
+++ divit.lua
@@ -59,20 +59,34 @@ function BlockQuote (elem)
 	local cont = elem.content
 	local className
 	local envType = cont[1].content[1].text
-	local refLabel
+	local refLabel = ''
+	
+	-- print(stringRep(cont,3))
 	
 	if (classNames[envType]) then
 		className = classNames[envType]
-		if (#cont[1].content > 1) and (cont[1].content[2].t == 'SoftBreak') then
-			-- print(stringRep(cont,3))
+		-- We hope that the environment we're trying to div has a label. Depending on the formatting of the tex, this can happen in a few ways. If the label appears on the same line of the tex as the \begin{env} then the first entry of cont (which is a table) is a Para, whose first contents item is a string containing the name of our environment, whose second contents item is a softbreak and whose third contents item is a span containing the label info.
+		if (#cont[1].content > 1) and (cont[1].content[2].t == 'SoftBreak') and (cont[1].content[3].t == 'Span') then
+			--In this case, remove the environment name
 			table.remove(cont[1].content,1)
+			--Remove the softbreak
 			table.remove(cont[1].content,1)
+			--Save the label name and then remove the corrsponding span. The resultant divs contents will be the remainder of cont 
 			refLabel = cont[1].content[1].attr.attributes.label
 			table.remove(cont[1].content,1)
-		else
+			
+		-- If the label appears on a new line in the tex, then cont has its first item as a Para which contains the string of the env name and as its second item a Para whose first contents item is a span containing the label info
+		elseif (cont[2].content[1].t == 'Span') then
+			--In this case, remove the environment name
 			table.remove(cont,1)
+			--Save the label name and then remove the corrsponding span. The resultant divs contents will be the remainder of cont 
 			refLabel = cont[1].content[1].attr.attributes.label
 			table.remove(cont[1].content,1)
+			
+		-- It may be that there is no label. In this case, we can't give the div an id. Cont will have its first item as a Para which contains the string of the env name.
+		else
+			--In this case, remove the environment name.
+			table.remove(cont,1)
 		end
 	end
 	return pandoc.Div(cont,{class=className,id=refLabel})
