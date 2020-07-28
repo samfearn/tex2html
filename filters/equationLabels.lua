e33bf5b467bc5e04d69b57a541758dfcc5068039
Sam Fearn <sam.fearn@gmail.com>
2020-07-28 18:30:08 +0100

    If aligned environments have labels, make them align rather than aligned

---
+++ filters/equationLabels.lua
@@ -0,0 +1,18 @@
+-- Load my stringRep module so it's available for inspecting the AST
+-- These seem to need to be loaded relative to the working firectory pandoc is called from
+-- local sr = require "filters/stringRep"
+
+function Math(elem)
+	-- inspect the AST using the following command
+	-- print(sr.stringRep(elem,3))
+	if (elem.mathtype == "DisplayMath") then
+		local math = elem.text
+		-- If the aligned environment contains a label, then turn it into align rather than aligned, otherwise leave it as aligned.
+		if math:find("\\label{[%w%-%+_&:%^]+}") then
+			math = (math:gsub("\\begin{aligned}","\\begin{align}"))
+			math = (math:gsub("\\end{aligned}","\\end{align}"))
+		end
+		elem.text = math
+	end
+	return elem
+end
